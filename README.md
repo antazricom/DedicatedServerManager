e8089d038ad1116f261dd248c4a6bc794a607495
Anthony <A4py5JK3@pm.me>
2021-12-09 23:04:54 +0100

    Project init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dedicated Server Manager
+Web application to handle operations on a personal dedicated server
---
+++ src/test/java/com/antazri/dedicatedservermanager/DedicatedServerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antazri.dedicatedservermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DedicatedServerManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
