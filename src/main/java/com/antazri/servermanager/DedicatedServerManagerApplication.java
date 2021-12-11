2c411da92d5bc2c70d6bcb2960053aa18b2da727
Anthony <A4py5JK3@pm.me>
2021-12-11 17:56:51 +0100

    Fix missing Spring Boot config annotations

--- src/main/java/com/antazri/servermanager/DedicatedServerManagerApplication.java
+++ src/main/java/com/antazri/servermanager/DedicatedServerManagerApplication.java
@@ -2,8 +2,12 @@ package com.antazri.servermanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(scanBasePackages = "com.antazri.servermanager")
+@EnableWebMvc
+@EnableJpaRepositories
 public class DedicatedServerManagerApplication {
 
     public static void main(String[] args) {
